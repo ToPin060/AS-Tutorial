b74dadeca8731607106f91d3566b068ef5c71f07
Tristan PINTO <tristan.pin81@gmail.com>
2023-11-30 11:44:52 +0100

    Resolves 7

--- app/src/main/java/helloandroid/ut3/com/helloandroid/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/com/helloandroid/MainActivity.java
@@ -1,23 +1,38 @@
 package helloandroid.ut3.com.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // on crée un nouveau TextView, qui est un widget permettant
-        // d'afficher du texte
         TextView tv = new TextView(this);
-
-        // configurer le texte à faire afficher par notre widget
         tv.setText("Hello, Android");
+        tv.setOnTouchListener(this);
 
         setContentView(tv);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (cnt != 10 && event.getAction() == MotionEvent.ACTION_DOWN)
+            cnt++;
+        else if (cnt == 10)
+            System.exit(RESULT_OK);
+
+        //float posx = event.getX();
+        //float posy = event.getY();
+
+        return true;
     }
 }
