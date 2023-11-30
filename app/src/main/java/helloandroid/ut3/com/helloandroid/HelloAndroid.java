a65107847c7e7f1ee3c85085a234ba59e68a2173
Tristan PINTO <tristan.pin81@gmail.com>
2023-11-30 10:57:13 +0100

    Resolves 3

--- app/src/main/java/helloandroid/ut3/com/helloandroid/HelloAndroid.java
+++ app/src/main/java/helloandroid/ut3/com/helloandroid/HelloAndroid.java
@@ -2,19 +2,32 @@ package helloandroid.ut3.com.helloandroid;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class HelloAndroid extends AppCompatActivity {
-    // on défini un handler qui représentera notre timer :
     private Handler mHandler;
+    private TextView tv;
+    private int cnt = 0;
 
     // un Runnable qui sera appelé par le timer
     private Runnable mUpdateTimeTask = new Runnable() {
         public void run() {
-            System.exit(RESULT_OK);
+            cnt++;
+
+            if (cnt < 5) {
+                updateContent("" + cnt);
+            }
+            else if (cnt == 5) {
+                updateContent("Hello, Android");
+            }
+            else {
+                System.exit(RESULT_OK);
+            }
+            mHandler.postDelayed(mUpdateTimeTask, 1000);
         }
     };
 
@@ -22,7 +35,13 @@ public class HelloAndroid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        tv = new TextView(this);
         mHandler = new Handler();
-        mHandler.postDelayed(mUpdateTimeTask, 5000);
+        mHandler.postDelayed(mUpdateTimeTask, 1000);
+    }
+
+    protected void updateContent(String text) {
+        tv.setText(text);
+        setContentView(tv);
     }
 }
